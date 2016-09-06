439498c6d04682651e96ac231239608a7a63dfa9
subhan <sub.metu@gmail.com>
2016-09-07 01:33:24 +0300

    First Commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var $root = $('html, body');
+
+$('.home-main a').hide();
+$('.home-main a').fadeIn(4000);
+
+//SCROLL ANIMATION
+$('.home-main a').click(function() {
+    $root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 1000);
+    return false;
+});
+
+
+//TEXTILATION
+$(function () {
+    $('.tlt').textillate(
+        { in: { effect: 'fadeInDownBig',
+                sync: false,
+                delay: 3
+              },
+        }
+    );
+})
