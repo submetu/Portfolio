19056aafab58f3ea250131b297b58d114c12cfcc
subhan <sub.metu@gmail.com>
2016-09-08 00:40:33 +0300

    UPDATE

--- js/app.js
+++ js/app.js
@@ -1,16 +1,21 @@
-
+var $root= $('html, body');
 
 $('.home-main a').hide();
 $('.home-main a').fadeIn(2000);
 
 //SCROLL ANIMATION
 $('.home-main a').click(function() {
-    $('html, body').animate({
+    $root.animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 2000);
     return false;
 });
-
+$('.recent-work .get-in-touch a').click(function() {
+    $root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 2000);
+    return false;
+});
 
 //TEXTILATION
 $(function () {
